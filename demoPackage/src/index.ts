d4f83e111b290f7781d90631da9a3104531ffe5b
IT-MikeS <20338451+IT-MikeS@users.noreply.github.com>
2022-03-03 15:32:38 -0500

    chore: fmt

--- demoPackage/src/index.ts
+++ demoPackage/src/index.ts
@@ -1,3 +1,5 @@
 export function capitalizeEachWord(sentence: string): string {
-  return sentence.replace(/(^\w{1})|(\s+\w{1})/g, letter => letter.toUpperCase());
+  return sentence.replace(/(^\w{1})|(\s+\w{1})/g, letter =>
+    letter.toUpperCase(),
+  );
 }
